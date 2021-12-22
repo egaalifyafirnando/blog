d29eb0172f5e4ff4e9e622ac70701d1d293f4d37
egaalfy <egaalifyaf@gmail.com>
2021-12-22 23:57:40 +0700

    progress CRUD category

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,11 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/dashboard', function () {
     return view('dashboard.index');
 })->middleware('auth');
+
+// dashboard post
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+// dashboard category
+Route::get('/dashboard/categories/checkSlug', [AdminCategoryController::class, 'checkSlug'])->middleware('admin');
 Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
